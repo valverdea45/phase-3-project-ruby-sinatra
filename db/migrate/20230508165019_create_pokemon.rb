317ba375886fbef7bb300eedc70de1d8b7652c65
Adrian <adrianvalverde45@gmail.com>
2023-05-08 12:59:17 -0400

    created pokemon table

--- db/migrate/20230508165019_create_pokemon.rb
+++ db/migrate/20230508165019_create_pokemon.rb
@@ -1,5 +1,8 @@
 class CreatePokemon < ActiveRecord::Migration[6.1]
   def change
-    
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :level
+      t.timestamps
   end
 end
