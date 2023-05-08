6c76796c80bd0e34f28516a74c75acdeb319dad2
Adrian <adrianvalverde45@gmail.com>
2023-05-08 13:08:46 -0400

    created pokemon skills table

--- db/migrate/20230508165052_create_pokemon_skill.rb
+++ db/migrate/20230508165052_create_pokemon_skill.rb
@@ -1,4 +1,11 @@
 class CreatePokemonSkill < ActiveRecord::Migration[6.1]
   def change
+    create_table :pokemon_skills do |t|
+      t.string :name
+      t.string :description
+      t.integer :power_points
+      t.integer :pokemon_id
+      t.timestamps
+    end
   end
 end
