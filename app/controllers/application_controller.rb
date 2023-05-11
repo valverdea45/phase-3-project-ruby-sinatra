a185f8bdb0db4e8fbb27c3aa047c88072046d6f4
Adrian <adrianvalverde45@gmail.com>
2023-05-11 15:05:12 -0400

    added post route for pokemon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
+  # post '/messages' do 
+  #   message = Message.create(
+  #     body: params[:body],
+  #     username: params[:username]
+  #   )
+  #   message.to_json
+  # end
+
   get "/pokemons" do
     pokemons = Pokemon.all
     pokemons.to_json
     # { message: "Good luck with your project!" }.to_json
+  end
+
+  post "/pokemons" do
+    pokemon = Pokemon.create(
+      name: params[:name],
+      level: params[:level],
+      sprite: params[:sprite]
+    )
+    pokemon.to_json
   end
 
 end
