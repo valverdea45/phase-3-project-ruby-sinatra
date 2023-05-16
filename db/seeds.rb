afa97ec47920d075bb3be0ddf57b5ccc0d4184cb
Adrian <adrianvalverde45@gmail.com>
2023-05-16 13:59:58 -0400

    updated seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 puts "🌱 Seeding spices..."
 require "pry"
 
@@ -7,16 +8,23 @@ require "pry"
 # pokemon_skills = ["Vine whip", "Watergun", "Tackle", "Mimic", "Curse", "Flamethrower"]
 
 
+ivysaur = Pokemon.create(name: "ivysaur", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")
+
+ivysaur.pokemon_skills.create(name: "Tackle", description: "", power_points: rand(5..30))
+
+ivysaur.pokemon_skills.create(name: "Vine Whip", description: "", power_points: rand(5..30))
+
+
 # pokemon_names.each do |pokemon|
 #     Pokemon.create(name: pokemon, level: rand(1..100))
 # end
 
-Pokemon.create(name: "ivysaur", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")
-Pokemon.create(name: "squitle", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png")
-Pokemon.create(name: "caterpie", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png")
-Pokemon.create(name: "weedle", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png")
-Pokemon.create(name: "pidgey", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png")
-Pokemon.create(name: "ekans", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png")
+# Pokemon.create(name: "ivysaur", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")
+# Pokemon.create(name: "squitle", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png")
+# Pokemon.create(name: "caterpie", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png")
+# Pokemon.create(name: "weedle", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png")
+# Pokemon.create(name: "pidgey", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png")
+# Pokemon.create(name: "ekans", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png")
 
 
 # bind one skill to one pokemon
@@ -30,17 +38,17 @@ Pokemon.create(name: "ekans", level: rand(1..100), sprite: "https://raw.githubus
 #     end
 # end
 
-PokemonSkill.create(name: "Vine Whip", description: "", power_points: rand(1..25), pokemon_id: 1)   
-PokemonSkill.create(name: "Water Gun", description: "", power_points: rand(1..25), pokemon_id: 2)
-PokemonSkill.create(name: "Tackle", description: "", power_points: rand(1..25), pokemon_id: 3)
-PokemonSkill.create(name: "Poison Sting", description: "", power_points: rand(1..25), pokemon_id: 4)
-PokemonSkill.create(name: "Wing attack", description: "", power_points: rand(1..25), pokemon_id: 5)
-PokemonSkill.create(name: "Bite", description: "", power_points: rand(1..25), pokemon_id: 6)
+# PokemonSkill.create(name: "Vine Whip", description: "", power_points: rand(1..25), pokemon_id: 1)   
+# PokemonSkill.create(name: "Water Gun", description: "", power_points: rand(1..25), pokemon_id: 2)
+# PokemonSkill.create(name: "Tackle", description: "", power_points: rand(1..25), pokemon_id: 3)
+# PokemonSkill.create(name: "Poison Sting", description: "", power_points: rand(1..25), pokemon_id: 4)
+# PokemonSkill.create(name: "Wing attack", description: "", power_points: rand(1..25), pokemon_id: 5)
+# PokemonSkill.create(name: "Bite", description: "", power_points: rand(1..25), pokemon_id: 6)
 
 
 
 
-binding.pry
+# binding.pry
 
 
 
