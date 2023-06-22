01ca4b2ee5bc758d53bd81ab299e20a1de5f4436
Adrian <adrianvalverde45@gmail.com>
2023-06-22 16:14:45 -0400

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,53 +2,16 @@
 puts "🌱 Seeding spices..."
 require "pry"
 
-# Seed your database here
-# pokemon_names = ["Bulbasaur", "Squirtle", "Pikachu", "Mew", "Gengar", "Charizard"]
-
-# pokemon_skills = ["Vine whip", "Watergun", "Tackle", "Mimic", "Curse", "Flamethrower"]
-
 
 ivysaur = Pokemon.create(name: "ivysaur", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")
 
-ivysaur.pokemon_skills.create(name: "Tackle", description: "", power_points: rand(5..30))
+ivysaur.pokemon_skills.create(name: "Tackle", description: "user charges enemy", power_points: rand(5..30))
 
-ivysaur.pokemon_skills.create(name: "Vine Whip", description: "", power_points: rand(5..30))
+ivysaur.pokemon_skills.create(name: "Vine Whip", description: "whips enemy with vine", power_points: rand(5..30))
 
+eevee = Pokemon.create(name: "Eevee", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png")
 
-# pokemon_names.each do |pokemon|
-#     Pokemon.create(name: pokemon, level: rand(1..100))
-# end
-
-# Pokemon.create(name: "ivysaur", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png")
-# Pokemon.create(name: "squitle", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png")
-# Pokemon.create(name: "caterpie", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png")
-# Pokemon.create(name: "weedle", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png")
-# Pokemon.create(name: "pidgey", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png")
-# Pokemon.create(name: "ekans", level: rand(1..100), sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png")
-
-
-# bind one skill to one pokemon
-# bind next skill to one pokemon
-
-# Pokemon.all.each do |pokemon|
-#     PokemonSkill.all.each do |skill|
-#         if skill.pokemon_id == pokemon_id
-#             PokemonSkill.create(name: "Vine Whip", description: "", power_points: rand(1..25), pokemon_id: pokemon.id)
-#         end
-#     end
-# end
-
-# PokemonSkill.create(name: "Vine Whip", description: "", power_points: rand(1..25), pokemon_id: 1)   
-# PokemonSkill.create(name: "Water Gun", description: "", power_points: rand(1..25), pokemon_id: 2)
-# PokemonSkill.create(name: "Tackle", description: "", power_points: rand(1..25), pokemon_id: 3)
-# PokemonSkill.create(name: "Poison Sting", description: "", power_points: rand(1..25), pokemon_id: 4)
-# PokemonSkill.create(name: "Wing attack", description: "", power_points: rand(1..25), pokemon_id: 5)
-# PokemonSkill.create(name: "Bite", description: "", power_points: rand(1..25), pokemon_id: 6)
-
-
-
-
-# binding.pry
+eevee.pokemon_skills.create(name: "Attract", description: "lows enemies defense", power_points: rand(5..30))
 
 
 
