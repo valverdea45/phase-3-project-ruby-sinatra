660c18d0273583399c97fd572da8646d526a2b4a
Adrian <adrianvalverde45@gmail.com>
2023-06-26 15:29:41 -0400

    got rid of notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-
-  # post '/messages' do 
-  #   message = Message.create(
-  #     body: params[:body],
-  #     username: params[:username]
-  #   )
-  #   message.to_json
-  # end
 
   get "/pokemons" do
     pokemons = Pokemon.all
     pokemons.to_json(include: :pokemon_skills)
-    # { message: "Good luck with your project!" }.to_json
   end
 
   post "/pokemons" do
@@ -23,12 +12,6 @@ class ApplicationController < Sinatra::Base
       level: params[:level],
       sprite: params[:sprite]
     )
-    # pokemon.pokemon_skills.create(
-    #   name: params[:skill_name],
-    #   description: params[:description],
-    #   power_points: params[:power_points]
-    # )
-    # binding.pry
 
     pokemon.to_json(include: :pokemon_skills)
   end
